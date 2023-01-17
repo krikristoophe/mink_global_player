12791fe320b005c4b695b42e1cb951b3775cf67a
Christophe Sonneville <contact@christophe-sonneville.fr>
2023-01-17 14:25:10 +0100

    add doc

--- lib/flow_player.dart
+++ lib/flow_player.dart
@@ -1,46 +1,71 @@
 import 'package:just_audio/just_audio.dart';
 
+/// Manage behaviour of audio player
 class FlowPlayer {
   final AudioPlayer _player;
+
+  /// AudioPlayer loopMode
+  /// Useful for make music loop infinite
   final LoopMode loopMode;
+
+  /// Volume used for current player
+  /// Updated at every [play] call
   double volume;
 
+  /// [mockedAudioPlayer] is audioPlayer used
+  /// Useful for testing
   FlowPlayer({
     AudioPlayer? mockedAudioPlayer,
-    this.loopMode = LoopMode.off,
+    this.loopMode = LoopMode.off, // Default no loop
     this.volume = 1,
   }) : _player = mockedAudioPlayer ?? AudioPlayer();
 
+  /// Load audio from [url] (network)
+  /// Stop current playing audio if any
   Future<void> loadUrl(String url) async {
     await stop();
     await _player.setUrl(url);
   }
 
+  /// Load audio from app assets
+  /// [asset] need to have full assets path
+  /// 
+  /// ```dart
+  /// loadAssets('assets/voice.mp3')
+  /// ```
   Future<void> loadAsset(String asset) async {
     await stop();
     await _player.setAsset(asset);
   }
 
+  /// Play current loaded audio
+  /// Update configuration (volume and loopMode)
   Future<void> play() async {
     await _player.setLoopMode(loopMode);
     await _player.setVolume(volume);
     await _player.play();
   }
 
+  /// Restart current loaded audio
+  /// Useful for effect like click sound that have to be played multiple time
   Future<void> restart() async {
     await stop();
     await _player.seek(Duration.zero);
     await play();
   }
 
+  /// Stop current audio played
   Future<void> stop() async {
     await _player.stop();
   }
 
+  /// Pause current audio played
   Future<void> pause() async {
     await _player.pause();
   }
 
+  /// Set volume of current flow
+  /// Only updated when calling [play] method
   Future<void> setVolume(double volume) async {
     this.volume = volume;
   }
--- lib/mink_global_player.dart
+++ lib/mink_global_player.dart
@@ -3,21 +3,32 @@ library mink_global_player;
 import 'package:just_audio/just_audio.dart';
 import 'package:mink_global_player/flow_player.dart';
 
+/// Manage the three audio flows
 class MinkGlobalPlayer {
+  /// Instance is used to keep same audio players for whole app
   static MinkGlobalPlayer instance = MinkGlobalPlayer();
 
+  /// Off voice flow
   final FlowPlayer offVoice;
+
+  /// Music flow, loop by default
   final FlowPlayer music;
+
+  /// Effects flow
   final FlowPlayer effect;
 
+  /// [mockedOffVoicePlayer], [mockedMusicPlayer] and [mockedEffectPlayer] are
+  /// only needed for testing
   MinkGlobalPlayer({
     FlowPlayer? mockedOffVoicePlayer,
     FlowPlayer? mockedMusicPlayer,
     FlowPlayer? mockedEffectPlayer,
   })  : offVoice = mockedOffVoicePlayer ?? FlowPlayer(),
-        music = mockedMusicPlayer ?? FlowPlayer(loopMode: LoopMode.all),
+        music = mockedMusicPlayer ??
+            FlowPlayer(loopMode: LoopMode.all), // default loop
         effect = mockedEffectPlayer ?? FlowPlayer();
 
+  /// Stop all flows players
   Future<void> stop() async {
     await Future.wait([
       offVoice.stop(),
@@ -26,6 +37,7 @@ class MinkGlobalPlayer {
     ]);
   }
 
+  /// Pause all flows players
   Future<void> pause() async {
     await Future.wait([
       offVoice.pause(),
@@ -34,6 +46,7 @@ class MinkGlobalPlayer {
     ]);
   }
 
+  /// Play all flow players
   Future<void> play() async {
     await Future.wait([
       offVoice.play(),
@@ -42,6 +55,7 @@ class MinkGlobalPlayer {
     ]);
   }
 
+  /// Play only music and off voice player
   Future<void> playOnlyMusicAndVoice() async {
     await Future.wait([
       offVoice.play(),
